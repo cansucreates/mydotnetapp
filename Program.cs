2805fb839fd4bc46f749b59024a1a792740e2e4d
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2024-02-12 23:12:32 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Krediler";
+            int term = 12;
+            double amount = 10000.5;
+
+            // variables --> camelCase
+            bool isAuthenticated = true;
+            Console.WriteLine(message);
+
+        }
+    }
+}
